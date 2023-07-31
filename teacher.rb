2227b697ae07d85f6df621b1f748954c5b249ee9
Enoisong <enobongitiaba988@gmail.com>
2023-07-30 17:18:27 -0700

    update teacher.rb file

--- teacher.rb
+++ teacher.rb
@@ -5,5 +5,10 @@ class Teacher < Person
         super(name, age, parent_permission)
         @specialization = specialization
     end
+    
+    def can_use_services?
+        true
+        end
+    end
 
  
