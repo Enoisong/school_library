1b146f826210062da7df7101078bf2b9fcc628af
Enoisong <enobongitiaba988@gmail.com>
2023-08-01 12:24:28 -0700

    made changes required by Reviewer

--- person.rb
+++ person.rb
@@ -1,7 +1,7 @@
 require_relative 'nameable'
 
 class Person < Nameable
-  attr_accessor :name, :age
+  attr_accessor :name, :rentals, :age
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
