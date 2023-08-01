3c1290c6d84f325e7f8fd9cd7ab57989ec39ef92
Enoisong <enobongitiaba988@gmail.com>
2023-08-01 12:35:07 -0700

    fix linters

--- person.rb
+++ person.rb
@@ -2,7 +2,7 @@ require_relative 'nameable'
 
 class Person < Nameable
   attr_accessor :name, :rentals, :age
-  attr_reader :id
+  attr_reader :id 
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     super()
